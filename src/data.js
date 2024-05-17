09535816868b334a57ba8eaa26036bb5a64bc394
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-05-16 19:55:50 -0500

    add basic route and dummy data

---
+++ src/data.js
@@ -0,0 +1,19 @@
+const shoppingList = [
+    {
+        name: "beer",
+        id: "jajasdb38bcesc",
+        checked: false
+    },
+    {
+        name: "arroz",
+        id: "jajabhckjs38bcesc",
+        checked: false
+    },
+    {
+        name: "acete",
+        id: "jajasdb38bcescasadas",
+        checked: true
+    },
+]
+
+export default shoppingList
---
+++ src/pages/ShoppingListPage.jsx
@@ -0,0 +1,8 @@
+export default function ShoppingListPage() {
+    
+    return (
+        <>
+            <h1>This is the shopping list page</h1>
+        </>
+    )
+}
