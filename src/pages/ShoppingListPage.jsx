dd8aef884f3c1b8a96f356363f19aee074562b6d
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-05-16 20:07:14 -0500

    add basic header

--- src/pages/ShoppingListPage.jsx
+++ src/pages/ShoppingListPage.jsx
@@ -1,8 +1,16 @@
+import { FaPlus } from "react-icons/fa6";
+
+
 export default function ShoppingListPage() {
     
     return (
         <>
-            <h1>This is the shopping list page</h1>
+            <header className="text-center py-2 text-lg font-bold grid grid-cols-9">
+                <h1 className="col-span-7 col-start-2">SHOPPING LIST</h1>
+                <button className="flex items-center justify-center">
+                    <FaPlus />
+                </button>
+            </header>
         </>
     )
 }
