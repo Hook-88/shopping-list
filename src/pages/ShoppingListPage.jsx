96305c52a98697561cc7b4cbb76e738aaad3714c
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-05-17 09:53:14 -0500

    add logic to check item in firestore

--- src/pages/ShoppingListPage.jsx
+++ src/pages/ShoppingListPage.jsx
@@ -1,12 +1,45 @@
 import { FaPlus, FaCheck } from "react-icons/fa6"
 
 import SHOPPINGLISTDATA from "../data"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Header from "../components/Header"
 import List from "../components/List/Index"
+import { doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore"
+import { db } from "../firebase/firebase"
 
 export default function ShoppingListPage() {
-    const [shoppingList, setShoppingList] = useState(SHOPPINGLISTDATA)
+    const [shoppingList, setShoppingList] = useState(null)
+    const docRef = doc(db, "shoppingList", "wA03LYangQz8a20aIKFV")
+
+    useEffect(() => {
+        const unsub = onSnapshot(docRef, snapshot => {
+            // sync up with local state
+            setShoppingList(snapshot.data().items)
+        })
+
+        return unsub
+    }, [])
+
+    async function toggleCheckedInFireStore(itemId) {
+        const docSnap = await getDoc(docRef)
+        const newArray = docSnap.data().items.map(item => {
+            if (item.id === itemId) {
+                
+                return {
+                    ...item,
+                    checked: !item.checked
+                }
+            } else {
+
+                return item
+            }
+        })
+        
+        await updateDoc(docRef, {
+            items: newArray         
+        })
+
+    }
     
     return (
         <>
@@ -18,32 +51,36 @@ export default function ShoppingListPage() {
             </Header>
 
             <main className="px-4">
-                <List>
-                    {
-                        shoppingList.map((item, index, arr) => {
-                            let liClassName = "flex items-center justify-between pr-4"
+                {
+                    shoppingList ?
+                    <List>
+                        {
+                            shoppingList.map((item, index, arr) => {
+                                let liClassName = "flex items-center justify-between pr-4 cursor-pointer"
 
-                            if (index !== (arr.length - 1)) {
-                                liClassName += " shadow-[rgba(100,100,100,0.3)_0px_1px_0px_0px]"
-                            }
+                                if (index !== (arr.length - 1)) {
+                                    liClassName += " shadow-[rgba(100,100,100,0.3)_0px_1px_0px_0px]"
+                                }
 
-                            if (item.checked) {
-                                liClassName += " text-white/30 line-through"
-                            }
+                                if (item.checked) {
+                                    liClassName += " text-white/30 line-through"
+                                }
 
-                            
-                            return (
-                                <List.Item 
-                                    key={item.id}
-                                    className={liClassName}
-                                >
-                                    {item.name}
-                                    {item.checked && <FaCheck className=""/>}
-                                </List.Item>
-                            )
-                        })
-                    }
-                </List>
+                                
+                                return (
+                                    <List.Item 
+                                        key={item.id}
+                                        className={liClassName}
+                                        onClick={() => toggleCheckedInFireStore(item.id)}
+                                    >
+                                        {item.name}
+                                        {item.checked && <FaCheck className=""/>}
+                                    </List.Item>
+                                )
+                            })
+                        }
+                    </List> : "Loading..."
+                }
             </main>
         </>
     )
