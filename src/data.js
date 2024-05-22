16dbb6e396f234d7a62ea401c44e6fd093c5ba1f
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-05-22 17:39:20 -0500

    add logic to check item

---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const ITEMS = [
+    { id: 'gro-001-milk', name: 'Milk', checked: false },
+    { id: 'gro-002-eggs', name: 'Eggs', checked: true },
+    { id: 'gro-003-bread', name: 'Bread', checked: false },
+    { id: 'gro-004-butter', name: 'Butter', checked: true },
+    { id: 'gro-005-cheese', name: 'Cheese', checked: false },
+    { id: 'gro-006-apples', name: 'Apples', checked: true },
+    { id: 'gro-007-bananas', name: 'Bananas', checked: false },
+    { id: 'gro-008-carrots', name: 'Carrots', checked: true },
+    { id: 'gro-009-lettuce', name: 'Lettuce', checked: false },
+    { id: 'gro-010-tomatoes', name: 'Tomatoes', checked: true }
+]
--- src/pages/ShoppingListPage.jsx
+++ src/pages/ShoppingListPage.jsx
@@ -2,39 +2,69 @@ import { FaPlus, FaCheck } from "react-icons/fa6"
 import PageHeader from "../components/PageHeader"
 import PageMain from "../components/PageMain"
 import PageLink from "../components/PageLink"
+import PageTitle from "../components/PageTitle"
 import Form from "../components/Form"
 import { useState } from "react"
+import { ITEMS } from "../data"
 
 export default function ShoppingListPage() {
     const [onAddItem, setOnAddItem] = useState(false)
+    const [shoppingList, setShoppingList] = useState(ITEMS)
 
     function toggleOnAddItem() {
         setOnAddItem(prevOnAddItem => !prevOnAddItem)
+    }
+
+    function toggleChecked(itemId) {
+        setShoppingList(prevShoppinglist => prevShoppinglist.map(item => item.id === itemId ? {...item, checked: !item.checked} : item))
     }
     
     return (
         <>
             <PageHeader>
-
-                <h1
-                    className="col-span-4 col-start-2 text-center font-bold"
-                >
-                    SHOPPING LIST
-                </h1>
+                <PageTitle>SHOPPING LIST</PageTitle>
 
                 <button
                     className="flex items-center justify-end pr-4"
                     onClick={toggleOnAddItem}
                 >
-                    {
-                        onAddItem ? <FaCheck /> : <FaPlus />
-                    }
+                    { onAddItem ? <FaCheck /> : <FaPlus /> }
                 </button>
 
             </PageHeader>
 
             <PageMain>
                 <PageLink to="recipes">Recipes</PageLink>
+
+                <ul
+                    className="bg-white/15 rounded-lg pl-4"
+                >
+                    {
+                        shoppingList.map((item, index, arr) => {
+                            let liCSS = "py-2 pr-4 flex items-center justify-between cursor-pointer"
+
+                            if ( index !== (arr.length - 1) ) {
+                                liCSS += " shadow-[rgba(100,100,100,0.5)_0px_1px_0px_0px]"
+                            }
+
+                            if (item.checked) {
+                                liCSS += " text-white/40 line-through"
+                            }
+
+                            return (
+                                <li
+                                    key={item.id}
+                                    className={liCSS}
+                                    onClick={() => toggleChecked(item.id)}
+                                >
+                                    {item.name}
+                                    {item.checked && <FaCheck />}
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+
                 {   
                     onAddItem &&
                     <Form
