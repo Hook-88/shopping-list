417d265327bab502e5db6f71ccb8d70768f04f57
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-05-23 22:55:55 -0500

    add basic layout

---
+++ src/pages/EditRecipeNamePage.jsx
@@ -0,0 +1,13 @@
+import PageHeader from "../components/PageHeader"
+import PageTitle from "../components/PageTitle"
+
+export default function EditRecipeNamePage() {
+    
+    return (
+        <>
+            <PageHeader>
+                <PageTitle>EDIT NAME</PageTitle>
+            </PageHeader>
+        </>
+    )
+}
