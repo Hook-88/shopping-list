f3d2b519dc955551a46b2ccb5630acdbe0566d88
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-05-24 09:30:07 -0500

    refactor code

--- src/layout/RecipeLayout.jsx
+++ src/layout/RecipeLayout.jsx
@@ -19,7 +19,6 @@ export default function RecipeLayout() {
         const newIngredientsArray = [...recipe.ingredients, ingredientObj]
 
         await updateDoc(docRef, {ingredients: newIngredientsArray})
-        
     }
 
     useEffect(() => {
