e2dd2d3cab1a3e3b49ee5705d28c12cfbafa2a1e
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-05-28 19:59:05 -0500

    add basic markup and styles for shoppinglist

--- src/pages/ShoppingListPage.jsx
+++ src/pages/ShoppingListPage.jsx
@@ -5,6 +5,7 @@ import Main from "../components/Main"
 import LinkButton from "../components/LinkButton"
 import Form from "../components/Form"
 import InputTekst from "../components/InputText"
+import { ITEMS } from "../data"
 
 import { useUIStore } from "../store/store"
 
@@ -26,6 +27,34 @@ export default function ShoppingListPage() {
             </Header>
 
             <Main>
+                <ul className="bg-white/15 col-span-12 rounded">
+                    {
+                        ITEMS.map((item, index, arr) => {
+                            let liClassName = "py-2 px-4 flex items-center justify-between cursor-pointer"
+
+                            if ( index !== (arr.length -1) ) {
+                                liClassName += " shadow-[rgba(100,100,100,0.5)_0px_1px_0px_0px]"
+                            }
+
+                            if (item.checked) {
+                                liClassName += " line-through text-white/30"
+                            }
+
+                            return (
+                                <li
+                                    key={item.id}
+                                    className={liClassName}
+                                >
+                                    {item.name}
+                                    {item.checked && <FaCheck />}
+                                </li>
+                            )
+                        })
+                    }
+                    {/* <li className="py-2 px-4 shadow-[rgba(100,100,100,0.5)_0px_1px_0px_0px]">Arroz</li>
+                    <li className="py-2 px-4 shadow-[rgba(100,100,100,0.5)_0px_1px_0px_0px]">Acete</li>
+                    <li className="py-2 px-4">Cervesa</li> */}
+                </ul>
                 {
                     showAddItem &&
                     <Form>
@@ -35,8 +64,8 @@ export default function ShoppingListPage() {
                     </Form>
                 }
 
-                <LinkButton to="recipes">Recipes</LinkButton>
-                
+                {!showAddItem && <LinkButton to="recipes">Recipes</LinkButton>}
+
             </Main>
         </>
     )
