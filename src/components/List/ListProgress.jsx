77fd2e8c8903afb3656196d102bdebfd4a603c7a
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-07-19 11:13:44 -0500

    imporved list progress

--- src/components/List/ListProgress.jsx
+++ src/components/List/ListProgress.jsx
@@ -19,6 +19,7 @@ export default function ListProgress({onClick = () => {}}) {
     return (
         <small onClick={handleClick}>
             {`(${checkedItemsArrLength}/${itemsArrLength})`}
+            { itemsArrLength === checkedItemsArrLength && " completed"}
         </small>
     )
 }
