c6c37d7151804fb4b458ae738055f9305c8500ab
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-07-23 18:35:49 -0500

    refactor code

--- src/pages/shopping_list/AddItemToListEl.jsx
+++ src/pages/shopping_list/AddItemToListEl.jsx
@@ -1,54 +1,13 @@
 import Card from "../../components/Card"
-import { forwardRef, useContext } from "react"
+import { forwardRef } from "react"
 import Banner from "../../components/Banner"
 import FormAddItem from "./FormAddItem"
-import useShoppingListHistory from "../../hooks/useShoppingListHistory"
-import { ShoppingListPageContext } from "./ShoppingListPage"
-import getStringFirstCharCap from "../../utility/getStringFirstCharCap"
-import addFirebaseDocToShoppingList from "../../firebase/utility/addFirebaseDocToShoppingList"
-import { useStore } from "../../store/store"
+import PopularItemsEl from "./PopularItemsEl"
 
 export default function AddItemToList(props, ref) {
-    const setBannerText = useStore(state => state.setBannerText)
-    const clearBanner = useStore(state => state.clearBanner)
-    const shoppingListHistory = useShoppingListHistory()
-    const { shoppingList } = useContext(ShoppingListPageContext)
-    const popularItems = getFilteredArray()
-
-    function getFilteredArray() {
-        const shoppingListStringArr = shoppingList?.map(item => item.name)
-        const shoppingListHistoryStringArr = shoppingListHistory?.map(historyItem => historyItem.name)
-        const filteredArr = shoppingListHistoryStringArr?.filter(item => {
-            if (shoppingListStringArr.includes(item)) {
-                return false
-            }
-
-            return true
-        })
-
-        return filteredArr
-    }
 
     function closeDialog() {
         props.onCancel()
-    }
-
-    function handleClick(itemName) {
-        const itemObj = {
-            name: itemName,
-            quantity: 1,
-            selected: false
-        }
-
-        addFirebaseDocToShoppingList(itemObj)
-        handleBanner(itemName)
-    }
-
-    function handleBanner(itemName) {
-        setBannerText(`${getStringFirstCharCap(itemName)} added to shopping list`)
-        setTimeout(() => {
-            clearBanner()
-        }, 1000)
     }
 
     return (
@@ -57,22 +16,10 @@ export default function AddItemToList(props, ref) {
                 <Banner />
 
                 <Card className="text-center text-white px-2 bg-[#1a1a1a] mt-4">
-                    <ul className="flex flex-wrap-reverse gap-2 mb-4">
-                        {
-                            popularItems?.slice(0, 5)
-                                .map(item => (
-                                    <li 
-                                        key={item}
-                                        className="border rounded py-2 px-4 flex-grow"
-                                        onClick={() => handleClick(item)}
-                                    >
-                                        {getStringFirstCharCap(item)}
-                                    </li>
-                                ))
-                        }
-                    </ul>
-                    <FormAddItem onCancel={closeDialog}/>
+                    <PopularItemsEl />
+                    <FormAddItem onCancel={closeDialog} />
                 </Card>
+
             </div>
         </dialog>
     )
---
+++ src/pages/shopping_list/PopularItemsEl.jsx
@@ -0,0 +1,64 @@
+import { useContext } from "react"
+import useShoppingListHistory from "../../hooks/useShoppingListHistory"
+import { ShoppingListPageContext } from "./ShoppingListPage"
+import getStringFirstCharCap from "../../utility/getStringFirstCharCap"
+import addFirebaseDocToShoppingList from "../../firebase/utility/addFirebaseDocToShoppingList"
+import { useStore } from "../../store/store"
+
+export default function PopularItemsEl() {
+    const setBannerText = useStore(state => state.setBannerText)
+    const clearBanner = useStore(state => state.clearBanner)
+    const shoppingListHistory = useShoppingListHistory()
+    const { shoppingList } = useContext(ShoppingListPageContext)
+    const popularItems = getFilteredArray()
+
+    function getFilteredArray() {
+        const shoppingListStringArr = shoppingList?.map(item => item.name)
+        const shoppingListHistoryStringArr = shoppingListHistory?.map(historyItem => historyItem.name)
+        const filteredArr = shoppingListHistoryStringArr?.filter(item => {
+            if (shoppingListStringArr.includes(item)) {
+                return false
+            }
+
+            return true
+        })
+
+        return filteredArr
+    }
+
+    function handleClick(itemName) {
+        const itemObj = {
+            name: itemName,
+            quantity: 1,
+            selected: false
+        }
+
+        addFirebaseDocToShoppingList(itemObj)
+        handleBanner(itemName)
+    }
+
+    function handleBanner(itemName) {
+        setBannerText(`${getStringFirstCharCap(itemName)} added to shopping list`)
+        setTimeout(() => {
+            clearBanner()
+        }, 1000)
+    }
+
+    return (
+        popularItems?.length > 0 ?
+        <ul className="flex flex-wrap-reverse gap-2 mb-4">
+            {
+                popularItems.slice(0, 5)
+                    .map(item => (
+                        <li 
+                            key={item}
+                            className="border rounded py-2 px-4 flex-grow"
+                            onClick={() => handleClick(item)}
+                        >
+                            {getStringFirstCharCap(item)}
+                        </li>
+                    ))
+            }
+        </ul> : null
+    )
+}
