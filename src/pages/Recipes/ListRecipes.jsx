4a0ff76fe280ec717a3ca554ab9fc3cd7ec4b65d
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-08-01 17:36:38 -0500

    add logic to display firebase recipes

---
+++ src/pages/Recipes/ListRecipes.jsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom"
+import PageHeader from "../../components/PageHeader"
+import { FaAngleRight } from "react-icons/fa6"
+import List from "../../components/List/List"
+import getStringFirstCharCap from "../../utility/getStringFirstCharCap"
+
+export default function ListRecipes({listItemArr}) {
+    
+    return (
+        <List>
+            <List.Body>
+                {
+                    listItemArr.map(recipe => (
+                        <List.Item key={recipe.id}>
+                            <Link
+                                to={recipe.id} 
+                                className="
+                                    border border-blue-700/60 bg-transparent text-blue-700 
+                                    flex items-center justify-between px-4 py-2 rounded
+                                "
+                            >
+                                {getStringFirstCharCap(recipe.name)}
+                                <FaAngleRight />
+                            </Link>
+                        </List.Item>
+                    ))
+                }
+                {/* <List.Item>
+                    <Link 
+                        className="
+                            border border-blue-700/60 bg-transparent text-blue-700 
+                            flex items-center justify-between px-4 py-2 rounded
+                        "
+                    >
+                        Arepas
+                        <FaAngleRight />
+                    </Link>
+                </List.Item> */}
+            </List.Body>
+        </List>
+    )
+}
--- src/pages/Recipes/RecipesPage.jsx
+++ src/pages/Recipes/RecipesPage.jsx
@@ -1,6 +1,63 @@
+import { Link } from "react-router-dom"
+import PageHeader from "../../components/PageHeader"
+import { FaAngleLeft, FaAngleRight, FaEllipsis } from "react-icons/fa6"
+import PageMain from "../../components/PageMain"
+import List from "../../components/List/List"
+import Card from "../../components/Card"
+import ListRecipes from "./ListRecipes"
+import Menu from "../../components/Menu/Menu"
+import { useEffect, useState } from "react"
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore"
+import { db } from "../../firebase/firebase"
+
 export default function RecipesPage() {
+    const [recipes, setRecipes] = useState(null)
+
+    useEffect(() => {
+        const collectionRef = collection(db, "recipes")
+        const q = query(collectionRef, orderBy("name"))
+        const unsub = onSnapshot(q, collectionSnapshot => {
+            const arr = collectionSnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }))
+
+            setRecipes(arr)
+        })
+
+        return unsub
+    }, [])
+
     
     return (
-        <h1>this is the recipes page</h1>
+        <>
+            <PageHeader>
+                <Link 
+                    className="flex items-center text-blue-700"
+                    to="./.."
+                >
+                    <FaAngleLeft />
+                    Back
+                </Link>
+                <h1 className="col-span-4 col-start-2 text-center">Recipes</h1>
+                <Menu>
+                    <Menu.Button className="justify-end">
+                        <FaEllipsis />
+                    </Menu.Button>
+                    <Menu.Dropdown className="right-0 top-10">
+                        <Menu.Item className="px-4 py-2 text-nowrap">
+                            Add Recipe
+                        </Menu.Item>
+                    </Menu.Dropdown>
+                </Menu>
+            </PageHeader>
+            <PageMain>
+                {
+                    recipes ? (
+                        <ListRecipes 
+                            listItemArr={recipes}
+                        />
+                    ) : "Loading..."
+                }
+                
+            </PageMain>
+        </>
     )
 }
