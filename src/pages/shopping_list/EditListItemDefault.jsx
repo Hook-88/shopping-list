3a922bb01712bbf7e937e7984be1c31c00ba8a75
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-08-01 07:50:51 -0500

    refactor code

--- src/pages/shopping_list/EditListItemDefault.jsx
+++ src/pages/shopping_list/EditListItemDefault.jsx
@@ -7,9 +7,6 @@ export default function EditListItemDefault({item}) {
     return (
         <Card className="flex items-center justify-between py-3">
             {getStringFirstCharCap(item.name)}
-            {/* {
-                item.quantity > 1 && ` (${item.quantity}x)`
-            } */}
             <span className="p-1.5 border border-transparent text-transparent">
                 <FaCheck />
             </span>
