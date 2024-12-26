43d7a9a9a077c3f47dd387522868f8ee430cb647
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-12-26 13:20:55 -0500

    add basic markup and styles

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,18 @@ import { RouterLink, RouterView } from 'vue-router'
 
     <RouterView />
 
+
     <footer class="text-center p-1">
       <small>&copy; 2024 Hook-88</small>
     </footer>
+    <!-- TODO ADD dialog ref to open en close -->
+    <dialog open class="bg-transparent h-screen">
+      <div class="backdrop backdrop-blur-sm h-full w-screen flex items-center justify-center">
+        <p class="bg-[##181818] border border-[#d1d2d3]/20 rounded-sm p-4 text-[#d1d2d3]">
+          here content
+        </p>
+      </div>
+    </dialog>
   </div>
 
 
