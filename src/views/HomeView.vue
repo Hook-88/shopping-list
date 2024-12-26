b9c4d4e16df4e661a5060a57968bc5334e4bd590
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-12-26 10:36:18 -0500

    refactor code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons/faCaretDown';
+
 </script>
 
 <template>
