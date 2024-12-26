d520c2fe339ed0e38af88146792d2addc00573d8
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-12-26 12:36:54 -0500

    refactor code

---
+++ src/components/DropdownMenu/DropdownMenu.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons/faBars'
+</script>
+
+<template>
+  <div class="relative grid">
+    <button class="flex items-center justify-end pr-2">
+      <FontAwesomeIcon :icon="faBars" />
+    </button>
+    <ul class="
+          absolute text-xl text-nowrap
+          border border-[#d1d2d3]/20 rounded-sm
+          right-1 top-9
+          backdrop backdrop-blur-sm
+        ">
+      <li class="px-4 py-2 border-b border-[#d1d2d3]/20">New List</li>
+      <li class="px-4 py-2 border-b border-[#d1d2d3]/20">Recipes</li>
+      <li class="px-4 py-2">Settings</li>
+    </ul>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import DropdownMenu from '@/components/DropdownMenu/DropdownMenu.vue';
 
 </script>
 
@@ -8,21 +7,7 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 
   <header class="text-2xl text-center p-2 border-b border-[#d1d2d3]/20 grid grid-cols-6">
     <h1 class="col-start-2 col-span-4">Shopping List</h1>
-    <div class="relative grid">
-      <button class="flex items-center justify-end pr-2">
-        <FontAwesomeIcon :icon="faBars" />
-      </button>
-      <ul class="
-          absolute text-xl text-nowrap
-          border border-[#d1d2d3]/20 rounded-sm
-          right-1 top-9
-          backdrop backdrop-blur-sm
-        ">
-        <li class="px-4 py-2 border-b border-[#d1d2d3]/20">New List</li>
-        <li class="px-4 py-2 border-b border-[#d1d2d3]/20">Recipes</li>
-        <li class="px-4 py-2">Settings</li>
-      </ul>
-    </div>
+    <DropdownMenu />
 
     <!-- <nav>
           <RouterLink to="/">Home</RouterLink>
