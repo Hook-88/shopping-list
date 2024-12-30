5a72ee27552b4dad4f2d7401d3b599c4cfa7307d
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-12-30 10:03:48 -0500

    refactor code

---
+++ src/data/data.ts
@@ -0,0 +1,62 @@
+export const GROCERIES = [
+  {
+    name: 'Apples',
+    selected: false,
+    quantity: 2,
+    id: 'henukdfwb236364',
+  },
+  {
+    name: 'Bananas',
+    selected: false,
+    quantity: 6,
+    id: 'jkuendxqw984757',
+  },
+  {
+    name: 'Milk',
+    selected: false,
+    quantity: 1,
+    id: 'wlenrtyui897653',
+  },
+  {
+    name: 'Bread',
+    selected: false,
+    quantity: 1,
+    id: 'zpmnxoqwe123450',
+  },
+  {
+    name: 'Eggs',
+    selected: false,
+    quantity: 12,
+    id: 'cvbnmasdf875432',
+  },
+  {
+    name: 'Cheese',
+    selected: false,
+    quantity: 1,
+    id: 'qwertyuiop654321',
+  },
+  {
+    name: 'Chicken Breast',
+    selected: false,
+    quantity: 4,
+    id: 'asdfghjkl987654',
+  },
+  {
+    name: 'Potatoes',
+    selected: false,
+    quantity: 5,
+    id: 'xcvbnm123456789',
+  },
+  {
+    name: 'Carrots',
+    selected: false,
+    quantity: 3,
+    id: 'lkjhgfds8765432',
+  },
+  {
+    name: 'Rice',
+    selected: false,
+    quantity: 1,
+    id: 'poiuytrew987654',
+  },
+]
