5167d373e20acac1342d06a99a353c499ab226c7
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2024-12-31 17:39:30 -0500

    add base item

---
+++ src/components/Lists/BaseItem.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface Item {
+  name: string,
+  id: string,
+}
+
+interface Props {
+  item: Item
+}
+
+defineProps<Props>()
+
+</script>
+
+<template>
+  <li>{{ item.name }}</li>
+</template>
---
+++ src/components/Lists/BaseList.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { Component } from 'vue';
+
+interface BaseItem {
+  name: string,
+  id: string
+}
+
+interface Props<T extends BaseItem> {
+  listItems: T[]
+  itemComponent: Component
+}
+
+
+defineProps<Props<BaseItem>>()
+
+</script>
+
+
+<template>
+  <ul class="space-y-2">
+    <component :is="itemComponent" v-for="(item, index) in listItems" :key="index" item:item />
+  </ul>
+</template>
