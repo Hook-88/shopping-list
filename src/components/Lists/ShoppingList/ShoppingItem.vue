05759a5c652c89ee8736e7607aab56258693fb97
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-01-02 09:35:53 -0500

    add logic to conditionally display quantity

--- src/components/Lists/ShoppingList/ShoppingItem.vue
+++ src/components/Lists/ShoppingList/ShoppingItem.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 interface Item {
   name: string
   id: string
@@ -14,5 +17,16 @@ defineProps<Props>()
 </script>
 
 <template>
-  <li class="border border-[#d1d2d3]/20 p-2 rounded-sm">{{ item.name }}</li>
+  <li class="border border-[#d1d2d3]/20 p-2 rounded-sm flex items-center">
+    <h3>{{ item.name }}</h3>
+    <span v-if="item.quantity > 1">({{ item.quantity }}x)</span>
+    <div class="flex gap-2 ml-auto">
+      <button class="px-2 py-1 rounded-sm bg-red-800">
+        <FontAwesomeIcon :icon="faMinus" />
+      </button>
+      <button class="px-2 py-1 rounded-sm bg-green-800">
+        <FontAwesomeIcon :icon="faPlus" />
+      </button>
+    </div>
+  </li>
 </template>
