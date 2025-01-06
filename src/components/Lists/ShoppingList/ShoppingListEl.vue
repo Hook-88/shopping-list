83900717daff4e23ba36ca8d5c44ede1fa014cdf
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-01-06 07:39:48 -0500

    refactor code

--- src/components/Lists/ShoppingList/ShoppingListEl.vue
+++ src/components/Lists/ShoppingList/ShoppingListEl.vue
@@ -11,56 +11,59 @@ const selectIdStore = useSelectId()
 
 //Delete items
 function handleClickDelete() {
-  selectIdStore.selectedIds.forEach(id => shoppingListStore.deleteItem(id))
-  selectIdStore.deSelectAll()
+    selectIdStore.selectedIds.forEach(id => shoppingListStore.deleteItem(id))
+    selectIdStore.deSelectAll()
 }
 
 const noSelection = computed(() => {
-  return selectIdStore.selectedIds.length === 0
+    return selectIdStore.selectedIds.length === 0
 })
 
 //Filter items
 const hasFilter = ref(false)
 
 const filterButtonText = computed(() => {
-  return hasFilter.value ? 'Show selected' : 'Hide selected'
+    return hasFilter.value ? 'Show selected' : 'Hide selected'
 })
 
 function handleClickFilter() {
-  hasFilter.value = !hasFilter.value
+    hasFilter.value = !hasFilter.value
 }
 
 //items to display
 const displayItems = computed(() => {
 
-  if (!shoppingListStore.items) {
-    throw new Error('Shopping list has no value')
-  }
+    if (!shoppingListStore.items) {
+        throw new Error('Shopping list has no value')
+    }
+    // if filter is applied
+    if (hasFilter.value) {
+        return shoppingListStore.items?.filter(item => {
+            if (!selectIdStore.selectedIds.some(id => id === item.id)) {
+                return item
+            }
+        })
+    }
 
-  if (hasFilter.value) {
-    return shoppingListStore.items?.filter(item => {
-      if (!selectIdStore.selectedIds.some(id => id === item.id)) {
-        return item
-      }
-    })
-  }
-
-  return shoppingListStore.items
+    return shoppingListStore.items
 })
+
+//progress
+
 
 </script>
 
 <template>
-  <div class="flex flex-col">
-    <header class="flex items-center justify-between">
-      <small class="text-sm">({{ selectIdStore.selectedIds.length }}/{{ shoppingListStore.items?.length
-        }})</small>
-      <button :disabled="noSelection" @click="handleClickFilter" class="text-sm p-1 disabled:text-white/40">{{
-        filterButtonText
-        }}</button>
-    </header>
-    <BaseList :item-component="ShoppingItem" :list-items="displayItems" />
-    <DangerButton class="mt-4" :disabled="noSelection" @click="handleClickDelete">Delete selection
-    </DangerButton>
-  </div>
+    <div class="flex flex-col">
+        <header class="flex items-center justify-between">
+            <small class="text-sm">({{ selectIdStore.selectedIds.length }}/{{ shoppingListStore.items?.length
+                }})</small>
+            <button :disabled="noSelection" @click="handleClickFilter" class="text-sm p-1 disabled:text-white/40">{{
+                filterButtonText
+                }}</button>
+        </header>
+        <BaseList :item-component="ShoppingItem" :list-items="displayItems" />
+        <DangerButton class="mt-4" :disabled="noSelection" @click="handleClickDelete">Delete selection
+        </DangerButton>
+    </div>
 </template>
