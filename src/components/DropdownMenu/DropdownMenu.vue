132fa31571eb64371a5ebd69762518948e3ffa89
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-01-17 11:17:27 -0500

    improved styles

--- src/components/DropdownMenu/DropdownMenu.vue
+++ src/components/DropdownMenu/DropdownMenu.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons/faBars'
-import { onMounted, onUnmounted, ref, useTemplateRef } from 'vue'
+import { computed, onMounted, onUnmounted, ref, useTemplateRef } from 'vue'
 
 const isOpen = ref<boolean>(false)
 const dropdownRef = useTemplateRef('dropdown')
+
+const props = defineProps<{
+  isOnBottomSreen?: boolean
+}>()
+
+//TODO computed prop for classname
+const positionClassName = computed(() => {
+  return props.isOnBottomSreen ? "right-1 bottom-14" : "right-1 top-9"
+})
 
 function toggleOpen() {
   isOpen.value = !isOpen.value
@@ -44,9 +53,8 @@ onUnmounted(() => {
     <ul @click="closeMenu" v-if="isOpen" class="
           absolute text-xl text-nowrap text-right
           border border-[#d1d2d3]/20 rounded-sm
-          right-1 top-9
           backdrop backdrop-blur-sm
-        ">
+        " :class="positionClassName">
       <slot></slot>
     </ul>
   </div>
--- src/components/toolbar/BaseToolbar.vue
+++ src/components/toolbar/BaseToolbar.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faPlus, faMinus, faTrashAlt, fa, faCaretDown } from '@fortawesome/free-solid-svg-icons'
+import DropdownMenu from '../DropdownMenu/DropdownMenu.vue'
+import MenuItem from '../DropdownMenu/MenuItem.vue'
+
 
 </script>
 
 <template>
-  <div class="border border-[#d1d2d3]/20 rounded-sm p-4 fixed inset-x-0 bottom-10 mx-4">
-    <p>here goes toolbar</p>
+  <div class="fixed inset-x-0 bottom-10 ml-2 flex gap-2">
+    <button class="px-4 py-2 rounded-sm bg-green-800 flex-grow">
+      <FontAwesomeIcon :icon="faPlus" />
+    </button>
+    <button class="px-4 py-2 rounded-sm bg-red-800 disabled:text-white/50 disabled:bg-red-800/50 flex-grow">
+      <FontAwesomeIcon :icon="faMinus" />
+    </button>
+    <DropdownMenu :is-on-bottom-sreen="true">
+      <template v-slot:button>
+        <FontAwesomeIcon :icon="faCaretDown" class="px-4 py-4 rounded-sm bg-sky-700" />
+      </template>
+      <MenuItem>Edit</MenuItem>
+      <MenuItem>Delete</MenuItem>
+
+    </DropdownMenu>
+
+    <!-- <button class="px-4 py-2 rounded-sm bg-sky-700">
+      <FontAwesomeIcon :icon="faCaretDown" />
+    </button> -->
   </div>
 </template>
