4b9aa043f33aa808a26abd570efa9021e8eeb602
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-01-17 16:36:49 -0500

    add new item button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -28,25 +28,14 @@ function handleClickNewItem() {
 
   <header class="text-2xl text-center p-2 border-b border-[#d1d2d3]/20 grid grid-cols-6">
     <h1 class="col-start-2 col-span-4">Shopping List</h1>
-    <DropdownMenu>
-      <MenuItem>
-      <button class="text-right" @click="handleClickNewItem">Add item</button>
-      </MenuItem>
-      <MenuItem>Recipes</MenuItem>
-      <MenuItem>Settings</MenuItem>
-    </DropdownMenu>
     <!-- <nav>
           <RouterLink to="/">Home</RouterLink>
           <RouterLink to="/about">About</RouterLink>
         </nav> -->
   </header>
 
+
   <main class="flex-grow px-2 flex flex-col gap-4">
-    <ShoppingListEl v-if="shoppingListStore.items && shoppingListStore.items.length > 0" />
-    <BaseButton @click="handleClickNewItem" v-else>
-      Add item
-      <FontAwesomeIcon :icon="faPlus" />
-    </BaseButton>
-    <BaseToolbar />
+    <BaseButton>Add item +</BaseButton>
   </main>
 </template>
