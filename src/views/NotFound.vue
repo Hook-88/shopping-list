ef6916823d4a52bc85cca577544822da56cf8d3a
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-01-22 18:07:47 -0500

    improved notfound styles

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons';
 </script>
 
 <template>
-  <h1>Oops!</h1>
-  <p>The page you're looking for does not exist.</p>
-  <RouterLink :to="{ name: 'Home' }">Back to home</RouterLink>
+  <main class="flex-grow px-2 flex flex-col gap-4 items-center">
+    <h1 class="text-center py-2 text-xl font-bold">Oops!</h1>
+    <p>The page you're looking for does not exist.</p>
+    <RouterLink class="text-center text-blue-600 border-b border-blue-600" :to="{ name: 'Home' }">
+      <FontAwesomeIcon :icon="faArrowLeftLong" />
+      Back to home
+
+    </RouterLink>
+  </main>
 </template>
