fe6f06d0414930a9edb55810d0ce921cabbfb32c
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-01-22 18:26:14 -0500

    add basic markup

---
+++ src/components/BaseList/BaseList.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { Component } from 'vue';
+
+
+defineProps<{
+  listItems: object
+  itemComponent: Component
+}>()
+
+</script>
+
+
+<template>
+  <ul>
+    <li v-for="(item, index) in listItems" :key="index">
+      <component :is="itemComponent" :item="item" v-bind="$attrs" />
+    </li>
+  </ul>
+</template>
