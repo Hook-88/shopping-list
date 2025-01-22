7a2d610a2fa7d491ad8d09b3b4d01b8ecd9f04d3
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-01-22 18:51:47 -0500

    improved styles shopping-item

--- src/components/ShoppingList/ShoppingItem.vue
+++ src/components/ShoppingList/ShoppingItem.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faGear } from '@fortawesome/free-solid-svg-icons'
+
+
 defineProps<{
   item: {
     name: string
@@ -9,8 +13,9 @@ defineProps<{
 </script>
 
 <template>
-  <div class="flex items-center border border-[#d1d2d3]/20 rounded p-2">
-    <p>{{ item.name }}</p>
+  <div class="flex items-center border border-[#d1d2d3]/20 rounded">
+    <p class="ml-3">{{ item.name }}</p>
     <span v-if="item.quantity > 1">&nbsp;({{ item.quantity }}x)</span>
+    <FontAwesomeIcon :icon="faGear" class="ml-auto p-4" />
   </div>
 </template>
