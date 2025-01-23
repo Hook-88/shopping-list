a238724eef383fce0b22e50cf8b2d37d3dadff89
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-01-22 19:25:05 -0500

    refactor code

--- src/components/ShoppingList/ShoppingList.vue
+++ src/components/ShoppingList/ShoppingList.vue
@@ -2,9 +2,23 @@
 import BaseList from '@/components/BaseList/BaseList.vue'
 import ShoppingItem from '@/components/ShoppingList/ShoppingItem.vue';
 import { SHOPPINGLIST } from '@/data'
+import { onMounted, ref } from 'vue';
+
+interface ShoppingItem {
+  name: string
+  quantity: number
+  label: string
+  id: string
+}
+
+const shopingListItem = ref<ShoppingItem[] | null>()
+
+onMounted(() => {
+  shopingListItem.value = SHOPPINGLIST
+})
 
 </script>
 
 <template>
-  <BaseList :list-items="SHOPPINGLIST" :item-component="ShoppingItem" />
+  <BaseList v-if="shopingListItem" :list-items="shopingListItem" :item-component="ShoppingItem" />
 </template>
