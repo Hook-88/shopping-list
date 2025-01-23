62be2d39ce4f327d17e4c234b337c050a0b3a4ff
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-01-23 14:45:27 -0500

    refactor code

---
+++ src/components/NavLink/NavLink.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { RouterLink } from 'vue-router'
+
+defineProps<{
+  linkTo: string
+}>()
+
+</script>
+
+<template>
+  <RouterLink class="py-2 px-4 border-b border-[#d1d2d3]/20 flex items-center justify-between tracking-wide"
+    :to="linkTo">
+    <slot></slot>
+    <FontAwesomeIcon :icon="faCaretRight" />
+  </RouterLink>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,46 +1,18 @@
 <script setup lang="ts">
 import ShoppingList from '@/components/ShoppingList/ShoppingList.vue'
 import { useDialogStore } from '@/stores/dialog';
-import { faCaretDown, faCaretRight, faCaretUp, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { computed, markRaw, onMounted, onUnmounted, ref } from 'vue';
+import { markRaw, onMounted } from 'vue';
 import AddNewItem from '@/components/Forms/AddNewItem/AddNewItem.vue';
 import { useShoppingList } from '@/stores/shoppingList'
 import BaseButton from '@/components/Buttons/BaseButton.vue';
-
-const navIsOpen = ref(false)
+import NavMenu from '@/components/Menu/NavMenu/NavMenu.vue';
+import NavLink from '@/components/NavLink/NavLink.vue'
 
 const dialogStore = useDialogStore()
 
 const shoppingListStore = useShoppingList()
-
-function handleClickOpenNav() {
-  navIsOpen.value = !navIsOpen.value
-}
-
-const openNavButtonIcon = computed(() => {
-  return navIsOpen.value ? faCaretUp : faCaretDown
-})
-
-function eventHandler(event: Event) {
-  if (!event.target) {
-    throw new Error('Event does not have a target')
-  }
-
-  const el = event.target as HTMLElement
-
-  if (!el.hasAttribute('popovertarget')) {
-    navIsOpen.value = false
-  }
-}
-
-onMounted(() => {
-  document.addEventListener('click', eventHandler)
-})
-
-onUnmounted(() => {
-  document.removeEventListener('click', eventHandler)
-})
 
 function handleClickAddNewItem() {
   dialogStore.open({
@@ -57,23 +29,11 @@ onMounted(() => {
 
 <template>
 
-  <header class="border-b border-[#d1d2d3]/20 flex" :class="navIsOpen && 'bg-[#1c1c1c]'">
-    <button class="py-2 px-4 text-xl text-left flex items-center gap-2 tracking-wider font-bold"
-      popovertarget="mypopover" @click="handleClickOpenNav">
-      Shopping List
-      <FontAwesomeIcon :icon="openNavButtonIcon" class="text-xl" />
-    </button>
-    <nav id="mypopover" popover class="border-b border-t border-[#d1d2d3]/20 border-b-[#d1d2d3]/20 p-0">
-      <RouterLink class="py-2 px-4 border-b border-[#d1d2d3]/20 flex items-center justify-between tracking-wide" to="#">
-        Recipes
-        <FontAwesomeIcon :icon="faCaretRight" />
-      </RouterLink>
-      <RouterLink class="py-2 px-4 flex items-center justify-between tracking-wide" to="/about">
-        About
-        <FontAwesomeIcon :icon="faCaretRight" />
-      </RouterLink>
-    </nav>
-
+  <header class="border-b border-[#d1d2d3]/20 flex">
+    <NavMenu>
+      <NavLink link-to="3">Recipes</NavLink>
+      <NavLink link-to="/about" class="border-none">About</NavLink>
+    </NavMenu>
     <button v-if="!dialogStore.isOpen" class="px-6 ml-auto text-xl" @click="handleClickAddNewItem">
       <FontAwesomeIcon :icon="faPlus" />
     </button>
@@ -86,18 +46,3 @@ onMounted(() => {
       new item</BaseButton>
   </main>
 </template>
-
-<style scoped>
-:popover-open {
-  width: 100%;
-  position: absolute;
-  /* inset: unset; */
-  display: flex;
-  flex-direction: column;
-  top: 2.75rem;
-  margin: 0;
-  background-color: #1c1c1c;
-  color: #d1d2d3;
-  font-size: 1.25rem;
-}
-</style>
