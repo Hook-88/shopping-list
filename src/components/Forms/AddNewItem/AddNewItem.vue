1d8fefa6cfe573eeababe9a8b6c6a3dbf464c2d6
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-01-23 12:38:19 -0500

    add logic to close modal on click cancel

--- src/components/Forms/AddNewItem/AddNewItem.vue
+++ src/components/Forms/AddNewItem/AddNewItem.vue
@@ -1,4 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useDialogStore } from '@/stores/dialog'
+
+const dialogStore = useDialogStore()
+
+function handleClickCancel() {
+  dialogStore.close()
+}
+
+</script>
 
 <template>
   <form class="text-[#d1d2d3] px-4 flex flex-col py-2 gap-3 pb-4">
@@ -25,7 +34,7 @@
 
     <div class="flex gap-2 mt-2">
       <button class="flex-grow bg-green-800 py-2 px-4">Add item</button>
-      <button class="bg-red-800 py-2 px-4">Cancel</button>
+      <button class="bg-red-800 py-2 px-4" type="button" @click="handleClickCancel">Cancel</button>
     </div>
   </form>
 </template>
