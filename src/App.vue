b8f7102be5b8cc22ff65eddeef132424c8de5234
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-01-23 11:45:49 -0500

    add dialog

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,11 @@ import { RouterLink, RouterView } from 'vue-router'
   <div class="h-screen flex flex-col gap-2">
 
     <RouterView />
+    <dialog open class="mt-11 w-full bg-[#1c1c1c] border-t border-b border-[#d1d2d3]/20">
+      <form>
+        <input type="text" placeholder="working...">
+      </form>
+    </dialog>
     <footer class="text-center p-1">
       <small>&copy; 2024 Hook-88</small>
     </footer>
