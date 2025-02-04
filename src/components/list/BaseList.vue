34fa413a291e191791e288be9c993ea93e994d61
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-02-04 18:13:22 -0500

    add header to list

--- src/components/list/BaseList.vue
+++ src/components/list/BaseList.vue
@@ -9,9 +9,14 @@ defineProps<{
 </script>
 
 <template>
-  <ul class="space-y-2">
-    <li v-for="(item, index) in listData" :key="index">
-      <component :is="listComponent" :item="item" />
-    </li>
-  </ul>
+  <div>
+    <header>
+      <button class="text-sm py-1">(4/8) - completed</button>
+    </header>
+    <ul class="space-y-2">
+      <li v-for="(item, index) in listData" :key="index">
+        <component :is="listComponent" :item="item" />
+      </li>
+    </ul>
+  </div>
 </template>
