ef2e00d9d8acc2f612bc2181cd02b7f2ecfebfd7
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-02-04 18:55:04 -0500

    add logic to select item

--- src/features/shoppinglist/ShoppingItem.vue
+++ src/features/shoppinglist/ShoppingItem.vue
@@ -1,21 +1,40 @@
 <script setup lang="ts">
+import { useSelectIdsStore } from '@/stores/selectIds';
 import { type GroceryItemInterface } from '@/types/GroceryItem'
 import { faCircle } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { computed } from 'vue';
 
-defineProps<GroceryItemInterface & {
-  isChecked?: boolean
-}>()
+const props = defineProps<GroceryItemInterface>()
+
+const selectIdsStore = useSelectIdsStore()
+
+const itemIdIsSelected = computed(() => selectIdsStore.selectedIds.some(selectedId => selectedId === props.id))
+
+const shoppingItemClassName = computed(() => {
+  if (itemIdIsSelected.value) {
+    return "flex items-center border border-white/20 rounded-xs bg-green-800"
+  }
+
+  return "flex items-center border border-white/20 rounded-xs"
+})
+
+function handleClickItem() {
+  if (itemIdIsSelected.value) {
+    selectIdsStore.removeId(props.id)
+    return
+  }
+  selectIdsStore.addId(props.id)
+}
+
 
 </script>
 
 <template>
-  <div class="flex items-center border border-white/20 rounded-xs" :class="{
-    ['bg-green-800']: isChecked
-  }">
+  <div :class="shoppingItemClassName" @click="handleClickItem">
     <h3 class="ml-4">{{ name }}</h3>
     &nbsp;
-    <span>(x)</span>
+    <span>({{ quantity }}x)</span>
     <button class="p-4 ml-auto">
       <FontAwesomeIcon :icon="faCircle" />
     </button>
