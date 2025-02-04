bb0eb82b9f7444b44548069b9e69ac0bf268f83e
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-02-04 16:52:29 -0500

    refacotr code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
 import { faCaretDown, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { RouterLink } from 'vue-router';
+
 </script>
 
 <template>
   <header class="py-2 flex justify-between border-b border-white/20">
-    <button class="px-2 text-2xl font-semibold tracking-wider">
+    <button class="px-2 text-2xl font-semibold tracking-wider" popovertarget="nav-menu">
       Lista de compras
       <FontAwesomeIcon :icon="faCaretDown" />
     </button>
+    <nav id="nav-menu" popover class="open:bg-sky-600">
+      <RouterLink to="#">Recipes</RouterLink>
+      <RouterLink :to="{ name: 'about' }">About</RouterLink>
+    </nav>
     <button class="px-4">
       <FontAwesomeIcon :icon="faPlus" />
     </button>
