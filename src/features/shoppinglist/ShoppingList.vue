10adf158a40eeab76bffb6528ac011106dad7ca5
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-02-04 19:06:52 -0500

    improved shoppinglist header

--- src/features/shoppinglist/ShoppingList.vue
+++ src/features/shoppinglist/ShoppingList.vue
@@ -2,17 +2,32 @@
 import BaseList from '@/components/list/BaseList.vue'
 import { type GroceryItemInterface } from '@/types/GroceryItem';
 import ShoppingItem from '@/features/shoppinglist/ShoppingItem.vue';
+import { useSelectIdsStore } from '@/stores/selectIds';
+import { computed } from 'vue';
 
-defineProps<{
+const selectIdsStore = useSelectIdsStore()
+
+const props = defineProps<{
   shoppingItems: GroceryItemInterface[]
 }>()
+
+const buttonText = computed(() => {
+  if (selectIdsStore.selectedIds.length === props.shoppingItems.length) {
+    return `(${selectIdsStore.selectedIds.length}/${props.shoppingItems.length}) - completed`
+  }
+
+  return `(${selectIdsStore.selectedIds.length}/${props.shoppingItems.length})`
+})
+
+
+
 </script>
 
 <template>
   <div>
     <header class="text-sm">
       <button class="py-1">
-        (4/{{ shoppingItems.length }})
+        {{ buttonText }}
       </button>
     </header>
     <BaseList :list-data="shoppingItems" :list-component="ShoppingItem" />
