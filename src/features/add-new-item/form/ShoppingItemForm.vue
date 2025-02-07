b2f0d6f43722e900a0d00724797ed707a717d32d
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-02-07 10:18:35 -0500

    add logic to close form

--- src/features/add-new-item/form/ShoppingItemForm.vue
+++ src/features/add-new-item/form/ShoppingItemForm.vue
@@ -2,13 +2,21 @@
 import BaseButton from '@/components/buttons/BaseButton.vue';
 import BaseInput from '@/components/input/BaseInput.vue';
 import BaseSelect from '@/components/input/BaseSelect.vue';
+import { useDialogStore } from '@/stores/dialog';
+
+const dialogStore = useDialogStore()
+
+function handleClickCancel() {
+  dialogStore.close()
+}
+
 
 </script>
 
 <template>
   <form class="flex flex-col gap-4 mt-2 w-full">
     <main class="mx-2 flex flex-col gap-2.5">
-      <BaseInput label="Item name" class="bg-white/10 rounded-sm py-1 px-2" />
+      <BaseInput label="Item name" class="bg-white/10 rounded-sm py-1 px-2" required />
       <div class="flex gap-2">
         <BaseInput :cancel-grow="true" label="Item quantity" class="bg-white/10 rounded-sm py-1 px-2 w-24"
           type="number" />
@@ -18,7 +26,7 @@ import BaseSelect from '@/components/input/BaseSelect.vue';
     </main>
     <footer class="flex items-center justify-between py-2 gap-2 border-t border-white/20 px-2">
       <BaseButton class="grow">Add item</BaseButton>
-      <BaseButton button-type="danger">Cancel</BaseButton>
+      <BaseButton button-type="danger" type="button" @click="handleClickCancel">Cancel</BaseButton>
     </footer>
   </form>
 </template>
