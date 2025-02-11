0b80646471a3e8b69549302cbffe46541b018547
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-02-11 07:22:11 -0500

    refactor code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,26 +2,17 @@
 import { faCaretDown, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { RouterLink } from 'vue-router';
-
+import MainNav from '@/components/main-nav/MainNav.vue';
 
 </script>
 
 
 <template>
   <header class="text-2xl tracking-wider font-bold border-b border-ash/20 flex justify-between">
-    <button class="flex items-center gap-1.5 py-1.5 px-2" popovertarget="main-nav">
-      <h1>Shopping list</h1>
-      <FontAwesomeIcon :icon="faCaretDown" />
-    </button>
-    <button class="py-1.5 px-2">
+    <MainNav />
+    <button class=" py-1.5 px-2">
       <FontAwesomeIcon :icon="faPlus" />
     </button>
-    <nav popover id="main-nav" class="open:flex open:flex-col mt-11 min-w-full bg-sky-950/50 text-ash tracking-wider">
-      <RouterLink to="/recipes" class="px-2 py-1.5 border-b border-ash/20">Recipes</RouterLink>
-      <RouterLink :to="{
-        name: 'about'
-      }" class="px-2 py-1.5 border-b border-ash/20">About</RouterLink>
-    </nav>
   </header>
   <main class="grow">
 
