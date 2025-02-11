c08110f0c2c9de2449b827e70a24786930a08161
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-02-11 07:53:59 -0500

    improved styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,7 @@ import BaseButton from '@/components/buttons/BaseButton.vue';
 import { onMounted } from 'vue';
 import { useShoppingListStore } from '@/stores/shoppingList';
 import { GROCERYITEMS } from '@/data/shoppingList';
+import { faCircle } from '@fortawesome/free-regular-svg-icons';
 
 const shoppingListStore = useShoppingListStore()
 
@@ -25,8 +26,14 @@ onMounted(() => {
     </BaseButton>
   </header>
   <main class="grow flex flex-col px-2">
-    <ul v-if="shoppingListStore.shoppingItems && shoppingListStore.shoppingItems.length > 0">
-      <li v-for="item in shoppingListStore.shoppingItems" :key="item.id">{{ item.name }}</li>
+    <ul v-if="shoppingListStore.shoppingItems && shoppingListStore.shoppingItems.length > 0" class="space-y-2">
+      <li v-for="item in shoppingListStore.shoppingItems" :key="item.id"
+        class="pl-2 border border-ash/20 rounded-xs flex items-center">
+        {{ item.name }}
+        <BaseButton class="ml-auto py-4 px-4 flex items-center">
+          <FontAwesomeIcon :icon="faCircle" />
+        </BaseButton>
+      </li>
     </ul>
     <BaseButton v-else button-type="action">Add new item</BaseButton>
 
