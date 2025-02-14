dd44634dd5db96bdb5cdd26f54f00e6864c2ff87
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-02-13 20:38:14 -0500

    refactor code

---
+++ src/components/inputs/BaseInput.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+interface Props {
+  label?: string
+  modelValue?: string | number
+}
+
+defineProps<Props>()
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string | number): void
+}>()
+
+function handleInput(event: Event) {
+  const target = event.target as HTMLInputElement
+  emit('update:modelValue', target.value)
+}
+
+</script>
+
+<template>
+  <div>
+    <label :for="label" v-if="label">{{ label }}:</label>
+    <br v-if="label">
+    <input v-bind="$attrs" class="w-full bg-ash/10 px-2 py-1 rounded-sm" :value="modelValue" @input="handleInput">
+  </div>
+</template>
--- src/features/add-new-item/form/AddNewItemForm.vue
+++ src/features/add-new-item/form/AddNewItemForm.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
 import BaseButton from '@/components/buttons/BaseButton.vue';
+import BaseInput from '@/components/inputs/BaseInput.vue';
 
 </script>
 
 <template>
   <form>
     <div class="flex flex-col m-3 gap-3">
-      <div>
-        <label for="">Item name:</label>
-        <br>
-        <input type="text" placeholder="item name..." class="w-full bg-ash/10 px-2 py-1 rounded-sm" required>
-      </div>
+      <BaseInput label="Item name" type="text" />
+
 
       <div class="flex gap-3">
         <div class="grow">
@@ -25,16 +23,16 @@ import BaseButton from '@/components/buttons/BaseButton.vue';
           <input type="string" placeholder="X" class="w-full bg-ash/10 px-2 py-1 rounded-sm">
         </div>
       </div>
-
-      <div>
+      <BaseInput label="Label" type="text" />
+      <!-- <div>
         <label for="">Label:</label>
         <br>
         <input type="text" placeholder="label" class="w-full bg-ash/10 px-2 py-1 rounded-sm">
-      </div>
+      </div> -->
     </div>
 
 
-    <div class="flex gap-3 border-t border-ash/20 p-2">
+    <div class="flex gap-3 border-t border-ash/20 p-3">
       <BaseButton button-type="action" class="grow">Add item</BaseButton>
       <BaseButton button-type="danger">Cancel</BaseButton>
     </div>
