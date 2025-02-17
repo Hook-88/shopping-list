476f5606ebdc5f0065d643ccc77d761c4d5ec31c
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-02-17 10:08:49 -0500

    add popover

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,8 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 <template>
   <header class="text-2xl tracking-wider border-b border-ash/20 flex justify-between">
-    <button class="p-2">
+
+    <button class="p-2" popovertarget="main-nav">
       Shopping List
       <FontAwesomeIcon :icon="faCaretDown" />
     </button>
@@ -17,7 +18,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
     <button class="p-2">
       <FontAwesomeIcon :icon="faPlus" />
     </button>
-    <nav class="flex flex-col mt-12.5 min-w-screen bg-obsidian text-ash text-lg" popover>
+    <nav class="open:flex flex-col mt-12.5 min-w-screen bg-obsidian text-ash text-lg" popover id="main-nav">
       <RouterLink to="/recipes" class="border-b border-ash/20 p-2 flex items-center justify-between">
         Recipes
         <FontAwesomeIcon :icon="faAngleRight" class="mr-1.5" />
