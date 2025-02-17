453779a9a98e9a06f1e9e95cbe28097b0b0e3efc
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-02-17 09:56:21 -0500

    add basic header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
+import { faCaretDown, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 
 
 </script>
 
 
 <template>
-  <header class="text-2xl tracking-wider p-2 border-b border-ash/20">
-    <h1>Shopping List</h1>
+  <header class="text-2xl tracking-wider border-b border-ash/20 flex justify-between">
+    <button class="p-2">
+      Shopping List
+      <FontAwesomeIcon :icon="faCaretDown" />
+    </button>
+
+    <button class="p-2">
+      <FontAwesomeIcon :icon="faPlus" />
+    </button>
   </header>
   <main class="grow">
 
