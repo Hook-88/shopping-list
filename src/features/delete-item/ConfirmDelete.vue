df962b392169ab7d6da763ef5e4f54df7ed7a099
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-02-17 09:22:44 -0500

    add logic to close dialog

--- src/features/delete-item/ConfirmDelete.vue
+++ src/features/delete-item/ConfirmDelete.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
-import BaseButton from '@/components/buttons/BaseButton.vue';
+import BaseButton from '@/components/buttons/BaseButton.vue'
+import { useDialogStore } from '@/stores/dialog';
+
+interface Props {
+  checkedIds: string[]
+}
+
+defineProps<Props>()
+
+const dialogStore = useDialogStore()
+
+function handleClickCancel() {
+  dialogStore.close()
+}
 
 </script>
 
@@ -11,6 +24,6 @@ import BaseButton from '@/components/buttons/BaseButton.vue';
   </main>
   <footer class="flex p-2 gap-2 border-t border-ash/20">
     <BaseButton button-type="action" class="grow">Confirm</BaseButton>
-    <BaseButton button-type="danger">Cancel</BaseButton>
+    <BaseButton button-type="danger" @click="handleClickCancel">Cancel</BaseButton>
   </footer>
 </template>
