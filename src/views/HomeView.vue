c138693fc93d9b57498c424112ea4cc09b28ccd3
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-02-17 10:07:55 -0500

    add basic marku and styles for nav menu

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { faCaretDown, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faAngleRight, faCaretDown, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 
@@ -17,6 +17,17 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
     <button class="p-2">
       <FontAwesomeIcon :icon="faPlus" />
     </button>
+    <nav class="flex flex-col mt-12.5 min-w-screen bg-obsidian text-ash text-lg" popover>
+      <RouterLink to="/recipes" class="border-b border-ash/20 p-2 flex items-center justify-between">
+        Recipes
+        <FontAwesomeIcon :icon="faAngleRight" class="mr-1.5" />
+      </RouterLink>
+      <RouterLink to="/about" class="border-b border-ash/20 p-2 flex items-center justify-between">
+        About
+        <FontAwesomeIcon :icon="faAngleRight" class="mr-1.5" />
+      </RouterLink>
+    </nav>
+
   </header>
   <main class="grow">
 
